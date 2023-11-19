841eed58662af2aed0fd03e92bb85fae77818fea
Daniel Slapman <danslapman@gmail.com>
2023-11-19 13:57:04 +0100

    Fix language-ext Properties & add tests

---
+++ src/LeviySoft.Visor.LanguageExt/Internal/Utils.cs
@@ -0,0 +1,22 @@
+﻿using LanguageExt;
+using System;
+
+namespace LeviySoft.Visor.LanguageExt.Internal;
+
+internal static class Utils
+{
+    public static int FindIndex<T>(this Seq<T> source, Func<T, bool> predicate)
+    {
+        int index = 0;
+        foreach (T elem in source)
+        {
+            if (predicate(elem))
+            {
+                return index;
+            }
+            ++index;
+        }
+
+        return -1;
+    }
+}
--- src/LeviySoft.Visor.LanguageExt/Property.cs
+++ src/LeviySoft.Visor.LanguageExt/Property.cs
@@ -1,5 +1,6 @@
 using LanguageExt;
 using System;
+using LeviySoft.Visor.LanguageExt.Internal;
 
 namespace LeviySoft.Visor.LanguageExt;
 
@@ -13,21 +14,24 @@ public static class Property
 
     public static IProperty<Seq<T>, T> First<T>() where T : class? => AtIndex<T>(0);
 
+    public static IProperty<Seq<T>, T> First<T>(Func<T, bool> predicate) where T : class? =>
+        Property<Seq<T>, T>.New(
+            seq => seq.FirstOrDefault(predicate),
+            upd => seq => {
+                var index = seq.FindIndex(predicate);
+                return Seq.map(seq, (i, v) => i == index ? upd(v) : v);
+            } 
+        );
+
     public static IProperty<Map<K, V>, V> AtKey<K, V>(K key) =>
         Property<Map<K, V>, V>.New(
-            map => map[key] ?? default,
+            map => map.ContainsKey(key) ? map[key] : default,
             upd => map => Map.map(map, (k, v) => Equals(k, key) ? upd(v) : v)
         );
 
     public static IProperty<Map<K, V>, V> AtKey<K, V>(K key, V defaultVal) =>
         Property<Map<K, V>, V>.New(
-            map => map[key] ?? default,
+            map => map.ContainsKey(key) ? map[key] : defaultVal,
             upd => map => map.AddOrUpdate(key, upd, upd(defaultVal))
-        );
-
-    public static IProperty<Seq<T>, T> First<T>(Func<T, bool> predicate) where T : class? =>
-        Property<Seq<T>, T>.New(
-            seq => seq.First(predicate),
-            upd => seq => Seq.map(seq, (_, v) => predicate(v) ? upd(v) : v)
         );
 }
