570bb25eb56603bf947de60dadb3f2bbb9e5cd4c
Daniel Slapman <danslapman@gmail.com>
2023-11-19 14:15:08 +0100

    Add chained test & usage example

--- src/LeviySoft.Visor.Gen.Tests/ChainedOpticTests.cs
+++ src/LeviySoft.Visor.Gen.Tests/ChainedOpticTests.cs
@@ -1,5 +1,7 @@
 using Shouldly;
 using Xunit;
+using System.Collections.Immutable;
+using LeviySoft.Visor.Collections.Immutable;
 
 namespace LeviySoft.Visor.Gen.Tests;
 
@@ -13,5 +15,16 @@ public class ChainedOpticTests
         var warehouse2 = Warehouse.FocusMain.FocusItem.IdLens.Set(42)(warehouse);
 
         warehouse2.Main.Item.Id.ShouldBe(42);
+    }
+
+    [Fact]
+    public void UpdateTagTest()
+    {
+        var warehouse = new Warehouse(new ItemWrapper(new InnerItem(1, "Default"), new Aux("test", ImmutableList.Create("tag1", "tag2"))));
+
+        var telescope = Warehouse.FocusMain.AuxLens.NotNull().Compose(Aux.TagsLens).Compose(Property.IImmutableList.First<string>());
+
+        var warehouse2 = telescope.Set("updated_tag")(warehouse);
+        warehouse2.Main.Aux?.Tags.ShouldBe(ImmutableList.Create("updated_tag", "tag2"));
     }
 }
